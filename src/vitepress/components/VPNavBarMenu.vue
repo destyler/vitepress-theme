e0cb0390311295535ae367fb5088f6e595cc5bbb
elonehoo <hi@elonehoo.me>
2024-09-26 22:56:09 +0800

    fix: update menu address

--- src/vitepress/components/VPNavBarMenu.vue
+++ src/vitepress/components/VPNavBarMenu.vue
@@ -1,21 +1,61 @@
 <script setup lang="ts">
+import type { Ref } from 'vue'
 import {
   NavigationItem,
   NavigationList,
   NavigationRoot,
   NavigationViewport,
 } from 'destyler'
+import { provide, ref, watch } from 'vue'
 import { useConfig } from '../composables/config'
 import VPNavBarMenuGroup from './VPNavBarMenuGroup.vue'
 import VPNavBarMenuLink from './VPNavBarMenuLink.vue'
 
 const { config } = useConfig()
+
+const menuModel = ref('')
+
+const action = ref<{ label: string, x: number }>()
+
+const triggerXList = ref<{ label: string, x: Ref<number> }[]>([])
+
+function handleSetTriggerX(label: string, x: Ref<number>) {
+  triggerXList.value.push({ label, x })
+}
+
+provide('vp-nav-bar-menu-trigger-x', {
+  handleSetTriggerX,
+  triggerXList,
+})
+
+const min = ref<number>(0)
+
+watch(menuModel, (value) => {
+  const numberLists = []
+  let feckAction: any = {}
+  for (const item of triggerXList.value) {
+    numberLists.push(item.x)
+    feckAction = triggerXList.value.filter(item => item.label === value)[0]
+  }
+  min.value = Math.min(...numberLists)
+  if (feckAction) {
+    action.value = feckAction
+  }
+}, {
+  immediate: true,
+  deep: true,
+})
 </script>
 
 <template>
-  <NavigationRoot>
+  <NavigationRoot v-model="menuModel" class="relative z-10 flex max-w-max flex-1 items-center justify-center">
     <NavigationList class="vp-nav-bar-menu">
-      <NavigationItem v-for="item in config.nav" :key="item.text" class="relative">
+      <NavigationItem
+        v-for="item in config.nav"
+        :key="item.text"
+        :value="item.text"
+        class="relative"
+      >
         <template v-if="'link' in item">
           <VPNavBarMenuLink :item="item" />
         </template>
@@ -24,18 +64,18 @@ const { config } = useConfig()
         </template>
       </NavigationItem>
     </NavigationList>
-    <div class="absolute left-0 top-full flex justify-center">
+    <div :style="{ '--left': `${action ? action.x - min : 0}px` }" class="absolute left-[--left] top-full flex justify-center">
       <NavigationViewport
         class="
-      origin-top-center relative mt-1.5
-      h-[var(--destyler-navigation-viewport-height)]
-      w-full overflow-hidden rounded-md border
-      bg-popover text-popover-foreground shadow-lg
-      data-[state=open]:animate-in
-      data-[state=closed]:animate-out
-      data-[state=closed]:zoom-out-95
-      data-[state=open]:zoom-in-90
-      md:w-[var(--destyler-navigation-viewport-width)]"
+        origin-top-center relative mt-1.5
+        h-[var(--destyler-navigation-viewport-height)]
+        w-full overflow-hidden rounded-md border
+        bg-popover text-popover-foreground shadow-lg
+        data-[state=open]:animate-in
+        data-[state=closed]:animate-out
+        data-[state=closed]:zoom-out-95
+        data-[state=open]:zoom-in-90
+        md:w-[var(--destyler-navigation-viewport-width)]"
       />
     </div>
   </NavigationRoot>
--- src/vitepress/components/VPNavBarMenuGroup.vue
+++ src/vitepress/components/VPNavBarMenuGroup.vue
@@ -1,23 +1,45 @@
 <script setup lang="ts">
 import type { NavItemWithChildren } from '../config'
+import { useElementBounding } from '@vueuse/core'
 import {
   Icon,
   NavigationContent,
   NavigationTrigger,
 } from 'destyler'
+import { inject, nextTick, useTemplateRef } from 'vue'
 
 const props = defineProps<{
   item: NavItemWithChildren
 }>()
+
+const { handleSetTriggerX } = inject('vp-nav-bar-menu-trigger-x')
+
+const triggerRef = useTemplateRef('trigger')
+const { x } = useElementBounding(triggerRef)
+handleSetTriggerX(props.item.text, x)
 </script>
 
 <template>
-  <NavigationTrigger class="vp-nav-bar-menu-button">
+  <NavigationTrigger ref="trigger" class="vp-nav-bar-menu-button">
     {{ props.item.text }}
     <Icon name="carbon:chevron-down" class="vp-nav-bar-menu-text-icon" />
   </NavigationTrigger>
-  <NavigationContent>
-    11
+  <NavigationContent
+    class="
+    left-0 top-0 w-full
+    data-[motion^=from-]:animate-in
+    data-[motion^=to-]:animate-out
+    data-[motion^=from-]:fade-in
+    data-[motion^=to-]:fade-out
+    data-[motion=from-end]:slide-in-from-right-52
+    data-[motion=from-start]:slide-in-from-left-52
+    data-[motion=to-end]:slide-out-to-right-52
+    data-[motion=to-start]:slide-out-to-left-52
+    md:absolute md:w-auto "
+  >
+    <ul class="grid w-[200px] gap-3 p-4 md:w-[200px] md:grid-cols-2 lg:w-[200px] ">
+      {{ props.item }}
+    </ul>
   </NavigationContent>
 </template>
 
@@ -30,6 +52,7 @@ const props = defineProps<{
 }
 
 .vp-nav-bar-menu-text-icon {
-  --at-apply: ml-1 w-14px h-14px fill-current;
+  --at-apply:
+    ml-1 w-14px h-14px fill-current;
 }
 </style>
