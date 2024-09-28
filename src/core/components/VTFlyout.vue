3647e6da5657e4162f3982176f2cd346a2fe1a27
elonehoo <hi@elonehoo.me>
2024-09-28 14:10:04 +0800

    feat: new component

---
+++ src/core/components/VTFlyout.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {
+  PopoverContent,
+  PopoverPortal,
+  PopoverRoot,
+  PopoverTrigger,
+} from 'destyler'
+
+const props = withDefaults(defineProps<{
+  contentWidth?: string
+}>(), {
+  contentWidth: '24rem',
+})
+</script>
+
+<template>
+  <ClientOnly>
+    <PopoverRoot>
+      <PopoverTrigger class="vt-flyout-trigger">
+        <slot name="vt-flyout-trigger" />
+      </PopoverTrigger>
+      <PopoverPortal>
+        <PopoverContent
+          side="bottom"
+          :side-offset="5"
+          class="vt-flyout-content"
+          :style="{ width: props.contentWidth }"
+          align="end"
+          as-child
+        >
+          <slot name="vt-flyout-content" />
+        </PopoverContent>
+      </PopoverPortal>
+    </PopoverRoot>
+    <slot />
+  </ClientOnly>
+</template>
---
+++ src/vitepress/components/VPNavBarExtra.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {
+  Icon,
+} from 'destyler'
+import { VTFlyout, VTSocialLinks } from '../../core'
+import { useConfig } from '../composables/config'
+
+const { config } = useConfig()
+</script>
+
+<template>
+  <VTFlyout content-width="12rem">
+    <template #vt-flyout-trigger>
+      <Icon name="carbon:overflow-menu-horizontal" class="vp-nav-bar-extra-icon vp-nav-bar-extra" />
+    </template>
+    <template #vt-flyout-content>
+      <div v-if="config.socialLinks" class="vt-menu-group">
+        <div class="vt-menu-item item">
+          <VTSocialLinks
+            class="social-links"
+            size="small"
+            :links="config.socialLinks"
+          />
+        </div>
+      </div>
+    </template>
+  </VTFlyout>
+</template>
+
+<style scoped>
+.vp-nav-bar-extra-icon{
+  --at-apply:
+    hidden md:block xl:hidden
+    w-20px h-20px cursor-pointer
+    text-foreground hover:text-foreground/60;
+}
+</style>
