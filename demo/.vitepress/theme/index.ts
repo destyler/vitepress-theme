a34d0388c543ef870ee59e2618abd8cdefb11650
elonehoo <hi@elonehoo.me>
2024-09-30 15:08:22 +0800

    fix: update demo

--- demo/.vitepress/theme/index.ts
+++ demo/.vitepress/theme/index.ts
@@ -18,8 +18,8 @@ export default {
       // 'content-top': () => h('h1', 'Announcement!'),
       // 'content-bottom': () => h('div', 'Some ads'),
       // 'aside-top': () => h('div', 'this could be huge'),
-      // 'aside-mid': () => h('div', { style: { height: '300px' }}, 'Sponsors'),
-      // 'aside-bottom': () => h('div', { style: { height: '300px' }}, 'Sponsors'),
+      // 'aside-mid': () => h('div', { style: { height: '300px' } }, 'Sponsors'),
+      // 'aside-bottom': () => h('div', { style: { height: '300px' } }, 'Sponsors'),
     })
   },
 }
